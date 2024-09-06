aaca3dc3c5ab8a5e201dc992d67b6d367c27aae9
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-09-06 12:28:55 +0200

    Adapt project for vercel deployment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require('./src/hello')
 
-app.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000')
-})
+// Supprime cette ligne car Vercel n'a pas besoin de spécifier un port
+// app.listen(3000, () => {
+//   console.log('Server listening on http://localhost:3000')
+// });
+
+// Exporte l'application Express pour que Vercel puisse la gérer
+module.exports = app
