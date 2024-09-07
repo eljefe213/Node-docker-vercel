bd429a94080385dc5415ce017a427d5594ba22b3
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-09-07 22:20:07 +0200

    Add root route for homepage

--- src/hello.js
+++ src/hello.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const { greet, bye } = require('../service/greetings')
 
+// Route pour la racine
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!')
+})
+
+// Route pour /hello
 app.get('/hello/:name?', (req, res) => {
   const name = req.params.name || '!'
   res.send(greet(name))
 })
 
+// Route pour /goodbye
 app.get('/goodbye/:name?', (req, res) => {
   const name = req.params.name || '!'
   res.send(bye(name))
